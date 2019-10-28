4356684186b8477d8f6266bc2e7f5a957981b907
Andrey Luiz <andreyluiz.developer@gmail.com>
2019-10-28 21:10:47 +0000

    Create a very basic bootstrap setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Navbar bg="light" expand="lg">
+			<Navbar.Brand href="/">SEF Monitor</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto"></Nav>
+				<Form inline>
+					<FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
+					<Button variant="outline-success">Buscar</Button>
+				</Form>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 }
 
 export default App;
